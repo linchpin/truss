0d8ddd4ae38b112b1e5208d2e100e930d2684a65
Maxwell Morgan <mmorgan@linchpin.agency>
2018-10-30 12:39:45 -0400

    Pan? Pam? Pan? No, Pin. Linchpin.

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-linchpan:app', () => {
+describe('generator-linchpin:app', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
