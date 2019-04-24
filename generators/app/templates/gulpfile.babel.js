791d6783e5ac66235c06f0e1630b71cc296ba397
Brian Fischer <bfischer@linchpin.agency>
2019-04-24 16:44:02 -0400

    Gulp File Update - Productions flags, LOCAL_PATH, minifying CSS/JS

--- generators/app/templates/gulpfile.babel.js
+++ generators/app/templates/gulpfile.babel.js
@@ -16,20 +16,61 @@ var autoprefixer  = require('autoprefixer');
 const $ = plugins();
 
 // Check for --production flag
-const PRODUCTION = !!(yargs.argv.production);
+let PRODUCTION = !!(yargs.argv.production);
 
 // Load settings from settings.yml
-const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
+const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS, LOCAL_PATH } = loadConfig();
 
+let sassConfig = {
+    mode: (PRODUCTION ? true : false)
+};
+
+// Define default webpack object
+let webpackConfig = {
+    mode: (PRODUCTION ? 'production' : 'development'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ "@babel/preset-env" ],
+                        compact: false
+                    }
+                }
+            }
+        ]
+    },
+    devtool: ! PRODUCTION && 'source-map'
+};
+
+/**
+ * Load in additional config files
+ */
 function loadConfig() {
-  let ymlFile = fs.readFileSync('config.yml', 'utf8');
-  return yaml.load(ymlFile);
+    let ymlFile = fs.readFileSync('config.yml', 'utf8');
+    return yaml.load(ymlFile);
+}
+
+/**
+ * Set production mode during the build process
+ *
+ * @param done
+ */
+function setProductionMode(done) {
+    PRODUCTION = false;
+    webpackConfig.mode = 'production';
+    webpackConfig.devtool = false;
+
+    sassConfig.production = true;
+    done();
 }
 
 // Build the "dist" folder by running all of the below tasks
 // Sass must be run later so UnCSS can search for used classes in the others assets.
 gulp.task('build:production',
-    gulp.series(clean, javascript, images, sass)); // removed copy from (javascript, images, copy)
+    gulp.series(setProductionMode, clean, javascript, images, sass)); // removed copy from (javascript, images, copy)
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -37,101 +78,81 @@ gulp.task('default',
 
 // This happens every time a build starts
 function clean(done) {
-  rimraf('css', done);
-  rimraf('js', done);
-  rimraf('images', done);
+    rimraf('css', done);
+    rimraf('js', done);
+    rimraf('images', done);
 }
 
 // Copy files out of the assets folder
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
 function copy() {
-  return gulp.src(PATHS.assets)
-    .pipe(gulp.dest('other/assets'));
+    return gulp.src(PATHS.assets)
+        .pipe(gulp.dest('other/assets'));
 }
 
 // In production, the CSS is compressed
 function sass() {
 
-  const postCssPlugins = [
-    // Autoprefixer
-    autoprefixer({ browsers: COMPATIBILITY }),
+    const postCssPlugins = [
+        // Autoprefixer
+        autoprefixer({ browsers: COMPATIBILITY }),
 
-    // UnCSS - Uncomment to remove unused styles in production
-    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
-  ].filter(Boolean);
+        // UnCSS - Uncomment to remove unused styles in production
+        // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
+    ].filter(Boolean);
 
-  return gulp.src('assets/scss/*.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      includePaths: PATHS.sass
-    })
-      .on('error', $.sass.logError))
-    .pipe($.postcss(postCssPlugins))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie10' })))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest('css'))
-    .pipe(browser.reload({ stream: true }));
+    return gulp.src('assets/scss/*.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            includePaths: PATHS.sass
+        })
+            .on('error', $.sass.logError))
+        .pipe($.postcss(postCssPlugins))
+        .pipe($.if(sassConfig.production, $.cleanCss({ compatibility: 'ie10' })))
+        .pipe($.if(!sassConfig.production, $.sourcemaps.write()))
+        .pipe(gulp.dest('css'))
+        .pipe(browser.reload({ stream: true }));
 }
-
-let webpackConfig = {
-  mode: (PRODUCTION ? 'production' : 'development'),
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ "@babel/preset-env" ],
-            compact: false
-          }
-        }
-      }
-    ]
-  },
-  devtool: !PRODUCTION && 'source-map'
-}
-
 
 // In production, the file is minified
 function javascript() {
-  return gulp.src(PATHS.entries)
-    .pipe(named())
-    .pipe($.sourcemaps.init())
-    .pipe(webpackStream(webpackConfig, webpack2))
-    .pipe($.if(PRODUCTION, $.uglify()
-      .on('error', e => { console.log(e); })
-    ))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest('js'));
+    return gulp.src(PATHS.entries)
+        .pipe(named())
+        .pipe($.sourcemaps.init())
+        .pipe(webpackStream(webpackConfig, webpack2))
+        .pipe($.if(PRODUCTION, $.uglify()
+                .on('error', e => { console.log(e); })
+))
+.pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+        .pipe(gulp.dest('js'));
 }
 
 // In production, the images are compressed
 function images() {
-  return gulp.src('assets/img/**/*')
-    .pipe($.if(PRODUCTION, $.imagemin([
-      $.imagemin.jpegtran({ progressive: true }),
-    ])))
-    .pipe(gulp.dest('images'));
+    return gulp.src('assets/img/**/*')
+        .pipe($.if(PRODUCTION, $.imagemin([
+            $.imagemin.jpegtran({ progressive: true }),
+        ])))
+        .pipe(gulp.dest('images'));
 }
 
 // Start a server with BrowserSync to preview the site in
 function server(done) {
     browser.init({
-        proxy : 'http://<%= text_domain %>.test'
+        proxy : LOCAL_PATH
     });
 }
 
 // Reload the browser with BrowserSync
 function reload(done) {
-  browser.reload();
-  done();
+    browser.reload();
+    done();
 }
 
 // Watch for changes to static assets, Sass, and JavaScript
 function watch() {
-  gulp.watch(PATHS.assets, copy);
-  gulp.watch('assets/scss/**/*.scss').on('all', sass);
-  gulp.watch('assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
-  gulp.watch('assets/img/**/*').on('all', gulp.series(images, browser.reload));
+    // gulp.watch(PATHS.assets, copy);
+    gulp.watch('assets/scss/**/*.scss').on('all', sass);
+    gulp.watch('assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
+    gulp.watch('assets/img/**/*').on('all', gulp.series(images, browser.reload));
 }
